ec2c3720fc7db14ca5e8185d8b33879574014eb6
Ali-Khlifi <alikhlifi.ig2017@gmail.com>
2023-06-11 15:53:35 +0200

    fix filmService

--- cinema-api/src/main/java/fr/uga/gestioncinema/service/IFilmService.java
+++ cinema-api/src/main/java/fr/uga/gestioncinema/service/IFilmService.java
@@ -4,12 +4,9 @@ import fr.uga.gestioncinema.entities.Film;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.Optional;
 
 public interface IFilmService {
     byte [] readImages(Long id) throws IOException;
     void saveFilmWithImage(Long id, MultipartFile imageFile);
 
-
-    Optional<Film> fetchOne(Long id);
 }
