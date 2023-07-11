074c8d123e5cbc795c190ab074bfb469e0189245
Ali-Khlifi <alikhlifi.ig2017@gmail.com>
2023-07-11 07:44:49 +0200

    Added getImageByFilm method and models to get data

--- cinema-client/src/app/components/film-projections/film-projections.component.ts
+++ cinema-client/src/app/components/film-projections/film-projections.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FilmProjectionService} from "../../services/film-projection/film-projection.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-film-projections',
   templateUrl: './film-projections.component.html',
   styleUrls: ['./film-projections.component.css']
 })
-export class FilmProjectionsComponent {
+export class FilmProjectionsComponent implements OnInit{
 
+  filmId: number = 1;
+  imageFilm!:any
+
+  constructor(private filmProjectionService: FilmProjectionService) {
+  }
+  ngOnInit(): void {
+    this.filmProjectionService.getImageByFilm(this.filmId).subscribe(data => {
+      let read = new FileReader();
+      read.readAsDataURL(data);
+      read.onloadend = () => {
+        this.imageFilm = read.result;
+      }
+    }, error => {
+      console.log(error);
+    });
+  }
 }
--- cinema-client/src/app/services/film-projection/film-projection.service.ts
+++ cinema-client/src/app/services/film-projection/film-projection.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FilmProjectionService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  urlApi = environment.API_URL;
+  urlImageFilm = this.urlApi + "/imageFilm"
+
+
+  public getImageByFilm(filmId: number){
+    return this.http.get(this.urlImageFilm + "/" + filmId, {responseType: 'blob'})
+  }
+
+  public getFilmProjectionBySalle(salleId: number){
+
+  }
+
+
 }
